4690f22205e45fd740becab72f9f7dc890d15670
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-02-22 18:38:27 -0800

    Make data compatible with existing puppet schema again.

--- main.php
+++ main.php
@@ -6,12 +6,13 @@ use Symfony\Component\Yaml\Yaml;
 use GoogleAPIExtensions\Groups;
 
 $groupData = file_get_contents(dirname(__FILE__) . "/sample_data/westkingdom.org.yaml");
-$existingState = Yaml::parse($groupData);
+$parsed = Yaml::parse($groupData);
+$existingState = $parsed['smartlist::subdomain_lists'];
 
 $groupManager = new Groups($existingState);
 
 $newState = $existingState;
-$newState['groups']['west']['lists']['webminister']['members'][] = 'new.admin@somewhere.com';
+$newState['west']['lists']['webminister']['members'][] = 'new.admin@somewhere.com';
 
 $auth = array();
 

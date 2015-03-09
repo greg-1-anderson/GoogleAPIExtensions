119251f52fe4f0e88e129a3c8df3505c411f5b12
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-03-08 23:41:32 -0700

    Fix authentication code: we need to provide a delegate user's email address for the service account's use.  Also add some sample API calls to fetch info about a group.

--- main.php
+++ main.php
@@ -49,16 +49,31 @@ if ($service_account_info) {
     $client->setAccessToken($_SESSION['service_token']);
   }
   $key = file_get_contents($key_file_location);
+
+  // https://www.googleapis.com/auth/admin.directory.group, https://www.googleapis.com/auth/admin.directory.group.readonly, https://www.googleapis.com/auth/admin.directory.group.member, https://www.googleapis.com/auth/admin.directory.group.member.readonly, https://www.googleapis.com/auth/apps.groups.settings, https://www.googleapis.com/auth/books
   $cred = new Google_Auth_AssertionCredentials(
     $service_account_name,
     array(
       Google_Service_Groupssettings::APPS_GROUPS_SETTINGS,
       Google_Service_Directory::ADMIN_DIRECTORY_GROUP,
       Google_Service_Directory::ADMIN_DIRECTORY_GROUP_READONLY,
+
+      Google_Service_Directory::ADMIN_DIRECTORY_GROUP_MEMBER,
+      Google_Service_Directory::ADMIN_DIRECTORY_GROUP_MEMBER_READONLY,
+
       Google_Service_Books::BOOKS,
     ),
-    $key
+    $key,
+    'notasecret'
   );
+  //
+  // Very important step:  the service account must also declare the
+  // identity (via email address) of a user with admin priviledges that
+  // it would like to masquerade as.  This is not well documented.
+  //
+  // See:  http://stackoverflow.com/questions/22772725/trouble-making-authenticated-calls-to-google-api-via-oauth
+  //
+  $cred->sub = $service_account_info['delegate-user-email'];
   $client->setAssertionCredentials($cred);
   if ($client->getAuth()->isAccessTokenExpired()) {
     $client->getAuth()->refreshTokenWithAssertion($cred);
@@ -84,19 +99,37 @@ print("Get directory service.\n");
 $service = new Google_Service_Directory($client);
 
 
+
 print("Call directory service:\n");
 
+// Get info about a group
 $data = $service->groups->get("west-webminister@westkingdom.org");
 var_export($data);
+print("\n");
 
-exit(0);
+// Get members of a group
+$data = $service->members->listMembers("west-webminister@westkingdom.org");
+var_export($data);
+print("\n");
 
+// List all the groups
+$opt = array('domain' => 'westkingdom.org');
+$data = $service->groups->listGroups($opt);
+var_export($data);
+print("\n");
+
+// The group settings APIs are still failing with an error:
+//   Domain cannot use Api, Groups service is not installed.
+// See: https://groups.google.com/forum/#!msg/google-apps-manager/EUz1aYmrnX4/zVe2tvnkqVoJ
+// Also, on this page: https://admin.google.com/westkingdom.org/AdminHome#GroupDetails:groupEmail=west-webminister%2540westkingdom.org&flyout=rolesPermissions
+// It says: If Groups for Business is activated later: The selected access level setting will include additional features
 $service = new Google_Service_Groupssettings($client);
 
 $data = $service->groups->get("west-webminister@westkingdom.org");
 //$data = $service->groups->get("02pta16n2up3ah0");
 
 var_export($data);
+print("\n");
 exit(0);
 
 $groupData = file_get_contents(dirname(__FILE__) . "/sample_data/westkingdom.org.yaml");

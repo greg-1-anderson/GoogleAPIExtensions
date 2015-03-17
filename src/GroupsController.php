6db5db111f2c8714f6ef79f89927cf90df75a0e6
Greg Anderson <greg.1.anderson@greeenknowe.org>
2015-03-17 13:35:13 -0700

    Rename some parameters in the Groups Controller interface.

--- src/GroupsController.php
+++ src/GroupsController.php
@@ -5,8 +5,8 @@ namespace Westkingdom\GoogleAPIExtensions;
 interface GroupsController {
   function insertBranch($branch);
   function deleteBranch($branch);
-  function insertMember($branch, $officename, $emailAddress);
-  function removeMember($branch, $officename, $emailAddress);
-  function insertOffice($branch, $officename, $settings);
-  function deleteOffice($branch, $officename, $settings);
+  function insertMember($branch, $officename, $memberEmailAddress);
+  function removeMember($branch, $officename, $memberEmailAddress);
+  function insertOffice($branch, $officename, $properties);
+  function deleteOffice($branch, $officename, $properties);
 }

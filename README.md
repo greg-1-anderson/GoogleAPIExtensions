9d73291f88b2b728b413ea7724f7243a51851851
Greg Anderson <greg.1.anderson@greeenknowe.org>
2015-03-18 21:10:50 -0700

    Add a Service Account Authenticator class, use it in our main.php file, and add some documentation to the README.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Group Management
-================
+== Group Management
+
 Callers may use this class to update their Google Groups memberships
 in bulk.  
 
@@ -14,8 +14,8 @@ API directly is that this way makes it easier to manage aliases for groups
 of groups, and other inter-group relationships, while minimizing the number
 of remote API calls to Google that need to be made.
 
-Running the Tests
-=================
+== Running the Tests
+
 This library contains a test suite that uses PHPUnit and Prophecy to
 insure that the classes provided here are correct.  The tests exercise
 the Google Apps APIs, but do not make any calls to Google, so it is
@@ -27,26 +27,130 @@ not necessary to set up any authentication credentials just to run the tests.
 
 All of the tests are also run on [Travis CI](https://travis-ci.org/westkingdom/google-api-extensions) on every commit.
 
-Using the Code
-==============
+== Using the Code
 
-Overview:
+=== Overview
 
-1. TODO: publish this library in Packagist so that it can be easily loaded from composer.json.
-1. [Set up your authorization information](http://docs.westkingdom.org/en/latest/google-api/)
+1. Include this Library Using Composer
+1. Set Up Your Authentication Information
 1. Prepare your data in $currentState and $newState
+1. Create a ServiceAccountAuthenticator
+1. Ask the service account authenticator to authenticate with the appropriate scopes
 1. Create a Standard Policy object
 1. Create a Google Client and authenticate with Google
-1. Create a Google Apps Groups Controller
-1. Tell the Groups Controller to update your group
+1. Create a Google Apps Groups Controller and Update It
 
-Example:
+=== Basic Example
+
+If you follow the instructions in the following sections, code similar to
+the basic overview shown below should work.
 ```
-$client = new Google_Client();
-$client->setApplicationName("My application");
-// Authenticate $client
+use Westkingdom\GoogleAPIExtensions\ServiceAccountAuthenticator;
+use Westkingdom\GoogleAPIExtensions\StandardGroupPolicy;
+use Westkingdom\GoogleAPIExtensions\GoogleAppsGroupsController;
+use Westkingdom\GoogleAPIExtensions\Groups;
+
+$authenticator = ServiceAccountAuthenticator("My application");
+$client = $authenticator->authenticate();
 $policy = new StandardGroupPolicy('mydomain.org');
 $controller = new GoogleAppsGroupsController($client, $policy);
-$groupManager = new Westkingdom\GoogleAPIExtensions\Groups($controller, $currentState);
+$groupManager = new Groups($controller, $currentState);
 $groupManager->update($newState);
 ```
+
+=== Include this Library Using Composer
+
+TODO: publish this library in Packagist so that it can be easily loaded 
+from composer.json, like this:
+
+{
+  "require": {
+    "westkingdom/googleapix": "~1"
+  }
+}
+
+To use Composer with popular content management systems, please see
+the following resources:
+
+- Drupal: [Composer Generate](https://www.drupal.org/project/composer_generate)
+- Joomla: [Getting Started with Composer and Joomla!](http://magazine.joomla.org/issues/issue-aug-2013/item/1450-getting-started-with-composer-and-joomla)
+- Wordpress: [Using Composer with WordPress](https://roots.io/using-composer-with-wordpress/)
+
+Of course, it is possible to use this library without composer; you just
+need to be responsible for setting up the autoloader, or including the
+class files yourself.  However, using Composer is strongly recommended.
+
+=== Set Up Your Authentication Information
+
+Follow the [authorization information setup instructions](http://docs.westkingdom.org/en/latest/google-api/) in the 
+documentation.
+
+=== Prepare Your Data
+
+This library expects you to accumulate all of the information about all
+of your groups, and their memberships in a nested heirarchical array.
+
+The structure is shown below in yaml, but you may store it in whatever
+format is most convenient for your application.
+
+=== Create a Service Authenticator
+
+A service authenticator will help your application load its authentication
+credential information from well-known files, so they do not need to be
+hard-coded in the application source code.
+
+`$authenticator = ServiceAccountAuthenticator("My application", $searchpath);`
+
+"My application" is the name of your application; this will be passed
+to any Google_Client created by the authenticator.
+
+`$searchpath` is an array of paths to search for authentication files.
+Relative paths are resolved relative to the current user's home directory.
+The default searchpath is:
+
+- .google-api
+- /etc/google-api
+
+=== Authenticate
+
+`$client = $authenticator->authenticate($serviceAccount, $scopes, $serviceToken);`
+
+=== Create a Standard Group Policy
+
+`$policy = new StandardGroupPolicy('mydomain.org', $defaults);`
+
+'mydomain.org' is the base domain for your Google Apps account.
+`$defaults` contain default values for properties used by the policy.
+
+=== Create a Google Apps Group Controller
+
+The Google Apps Group Controller is the object that actually talks
+to the Google API.  Batch mode is always used; you can manage the
+batch object yourself, as shown below:
+
+```
+$client->setUseBatch(true);
+$batch = new \Google_Http_Batch($client);
+$controller = new GoogleAppsGroupsController($client, $policy, $batch);
+...
+// When finished:
+$batch->execute();
+```
+
+If you do not want to manage the batch object, just leave off those
+lines, and the contorller will create and execute batches as needed.
+
+=== Create a Groups Object and Update It
+
+The Groups object is responsible for evaluating how the new state
+differs from the current state.  It then instructs the controller to make
+whatever changes are necessary to update the current state to match
+the new state.
+
+```
+$groupManager = new Groups($controller, $currentState);
+$groupManager->update($newState);
+```
+Changes are always made in batch mode.  Batch mode can be handled for
+you, or you can control it yourself, as shown in the previous section.
+
--- main.php
+++ main.php
@@ -4,101 +4,43 @@ include dirname(__FILE__) . "/vendor/autoload.php";
 
 use Symfony\Component\Yaml\Yaml;
 use GoogleAPIExtensions\Groups;
+use Westkingdom\GoogleAPIExtensions\ServiceAccountAuthenticator;
 
-// Look up our API key, if we can find it.
-$api_key = FALSE;
-$home = $_SERVER['HOME'];
-foreach (array("$home/.google-api/server.key", "/etc/google-api/server.key") as $key_file) {
-  if (file_exists($key_file)) {
-    $api_key = trim(file_get_contents($key_file));
-  }
-}
+$scopes = array(
+    // Books is only for testing.  The rest I think we actually need.
+    Google_Service_Books::BOOKS,
 
-// Look up our service account, if we can find it.
-$service_account_info = FALSE;
-foreach (array("$home/.google-api/service-account.yaml", "/etc/google-api/service-account.yaml") as $service_account_info_file) {
-  if (file_exists($service_account_info_file)) {
-    $service_account_info = Yaml::parse($service_account_info_file);
-    if ($service_account_info['key-file'][0] != '/') {
-      $service_account_info['key-file'] = dirname($service_account_info_file) . '/' . basename($service_account_info['key-file']);
-    }
-  }
-}
+    Google_Service_Groupssettings::APPS_GROUPS_SETTINGS,
 
-// Create a new google client.  We need this for all API access.
-$client = new Google_Client();
-$client->setApplicationName("Google Group Test");
+    Google_Service_Directory::ADMIN_DIRECTORY_GROUP,
+    Google_Service_Directory::ADMIN_DIRECTORY_GROUP_READONLY,
 
-// If we have an API key, that will give us a certain amount of access.
-if ($api_key) {
-  $client->setDeveloperKey($api_key);
-}
+    Google_Service_Directory::ADMIN_DIRECTORY_GROUP_MEMBER,
+    Google_Service_Directory::ADMIN_DIRECTORY_GROUP_MEMBER_READONLY,
 
-// If we have a service account, that will give us even more access.
-if ($service_account_info) {
-  $client_id = $service_account_info['client-id'];
-  $service_account_name = $service_account_info['email-address'];
-  $key_file_location = $service_account_info['key-file'];
-  $key_file_password = $service_account_info['key-file-password'];
+    Google_Service_Directory::ADMIN_DIRECTORY_NOTIFICATIONS,
 
-  if (isset($_SESSION['service_token'])) {
-    $client->setAccessToken($_SESSION['service_token']);
-  }
-  $key = file_get_contents($key_file_location);
+    Google_Service_Directory::ADMIN_DIRECTORY_ORGUNIT,
+    Google_Service_Directory::ADMIN_DIRECTORY_ORGUNIT_READONLY,
 
-  // https://www.googleapis.com/auth/books, https://www.googleapis.com/auth/admin.directory.group, https://www.googleapis.com/auth/admin.directory.group.readonly, https://www.googleapis.com/auth/admin.directory.group.member, https://www.googleapis.com/auth/admin.directory.group.member.readonly, https://www.googleapis.com/auth/apps.groups.settings, https://www.googleapis.com/auth/admin.directory.notifications, https://www.googleapis.com/auth/admin.directory.orgunit, https://www.googleapis.com/auth/admin.directory.orgunit.readonly, https://www.googleapis.com/auth/admin.directory.user, https://www.googleapis.com/auth/admin.directory.user.alias, https://www.googleapis.com/auth/admin.directory.user.alias.readonly, https://www.googleapis.com/auth/admin.directory.user.readonly, https://www.googleapis.com/auth/admin.directory.user.security, https://www.googleapis.com/auth/admin.directory.userschema, https://www.googleapis.com/auth/admin.directory.userschema.readonly, https://www.googleapis.com/auth/calendar, https://www.googleapis.com/auth/calendar.readonly
-  $cred = new Google_Auth_AssertionCredentials(
-    $service_account_name,
-    array(
-      // Books is only for testing.  The rest I think we actually need.
-      Google_Service_Books::BOOKS,
+    Google_Service_Directory::ADMIN_DIRECTORY_USER,
+    Google_Service_Directory::ADMIN_DIRECTORY_USER_READONLY,
 
-      Google_Service_Groupssettings::APPS_GROUPS_SETTINGS,
+    Google_Service_Directory::ADMIN_DIRECTORY_USER_ALIAS,
+    Google_Service_Directory::ADMIN_DIRECTORY_USER_ALIAS_READONLY,
 
-      Google_Service_Directory::ADMIN_DIRECTORY_GROUP,
-      Google_Service_Directory::ADMIN_DIRECTORY_GROUP_READONLY,
+    Google_Service_Directory::ADMIN_DIRECTORY_USER_SECURITY,
 
-      Google_Service_Directory::ADMIN_DIRECTORY_GROUP_MEMBER,
-      Google_Service_Directory::ADMIN_DIRECTORY_GROUP_MEMBER_READONLY,
+    Google_Service_Directory::ADMIN_DIRECTORY_USERSCHEMA,
+    Google_Service_Directory::ADMIN_DIRECTORY_USERSCHEMA_READONLY,
 
-      Google_Service_Directory::ADMIN_DIRECTORY_NOTIFICATIONS,
-
-      Google_Service_Directory::ADMIN_DIRECTORY_ORGUNIT,
-      Google_Service_Directory::ADMIN_DIRECTORY_ORGUNIT_READONLY,
-
-      Google_Service_Directory::ADMIN_DIRECTORY_USER,
-      Google_Service_Directory::ADMIN_DIRECTORY_USER_READONLY,
-
-      Google_Service_Directory::ADMIN_DIRECTORY_USER_ALIAS,
-      Google_Service_Directory::ADMIN_DIRECTORY_USER_ALIAS_READONLY,
-
-      Google_Service_Directory::ADMIN_DIRECTORY_USER_SECURITY,
-
-      Google_Service_Directory::ADMIN_DIRECTORY_USERSCHEMA,
-      Google_Service_Directory::ADMIN_DIRECTORY_USERSCHEMA_READONLY,
-
-      Google_Service_Calendar::CALENDAR,
-      Google_Service_Calendar::CALENDAR_READONLY,
-
-    ),
-    $key,
-    $key_file_password
+    Google_Service_Calendar::CALENDAR,
+    Google_Service_Calendar::CALENDAR_READONLY,
   );
 
-  //
-  // Very important step:  the service account must also declare the
-  // identity (via email address) of a user with admin priviledges that
-  // it would like to masquerade as.  This is not well documented.
-  //
-  // See:  http://stackoverflow.com/questions/22772725/trouble-making-authenticated-calls-to-google-api-via-oauth
-  //
-  $cred->sub = $service_account_info['delegate-user-email'];
-  $client->setAssertionCredentials($cred);
-  if ($client->getAuth()->isAccessTokenExpired()) {
-    $client->getAuth()->refreshTokenWithAssertion($cred);
-  }
-  $_SESSION['service_token'] = $client->getAccessToken();
-}
+$authenticator = new ServiceAccountAuthenticator("Google Group API Test");
+$client = $authenticator->authenticate('service-account.yaml', $scopes);
+
 
 // Test the actual Google API
 
@@ -132,7 +74,7 @@ var_export($data);
 print("\n");
 
 // TEMPORARY
-$service->users->delete("first.last@$domain");
+// $service->users->delete("first.last@$domain");
 
 $user = new Google_Service_Directory_User(array(
     'name' => array(

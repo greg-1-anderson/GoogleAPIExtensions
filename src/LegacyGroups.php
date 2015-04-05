61f9fe9cb74babd8258c9bdeb886d1f2296d9384
Greg Anderson <greg.1.anderson@greeenknowe.org>
2015-04-05 15:13:23 -0700

    Add legacy group data conversion class.

---
+++ src/LegacyGroups.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Westkingdom\GoogleAPIExtensions;
+
+class LegacyGroups {
+
+  static function parseLegacyDreamHostGroups($dreamhostGroups, $blacklist = array()) {
+    $legacy = array();
+    foreach (explode("\n", $dreamhostGroups) as $group) {
+      // Replace all runs of spaces with a single space
+      $group = trim(preg_replace('/  */', ' ', $group));
+      if (!empty($group) && ($group[0] != '#')) {
+        list($emailAddress, $members) = explode(' ', $group, 2);
+        if (!in_array($emailAddress, $blacklist)) {
+          $members = array_diff(explode(',', $members), $blacklist);
+          if (!empty($members)) {
+            $legacy[$emailAddress] = $members;
+          }
+        }
+      }
+    }
+    return $legacy;
+  }
+
+  static function applyLegacyGroups($memberships, $legacy) {
+    foreach ($legacy as $legacyGroup => $members) {
+      $matchedExisting = LegacyGroups::applyLegacyGroup($memberships, $legacyGroup, $members);
+      if (!$matchedExisting) {
+        $legacyOfficename = preg_replace('/@.*/', '', $legacyGroup);
+        $memberships['_legacy']['lists'][$legacyOfficename] = array(
+          'members' => $members,
+          'properties' => array(
+            'group-email' => $legacyGroup,
+          ),
+        );
+      }
+    }
+    return $memberships;
+  }
+
+  static protected function applyLegacyGroup(&$memberships, $legacyGroup, $members) {
+    foreach ($memberships as $branch => $officesLists) {
+      if ($branch[0] != '#') {
+        $offices = $officesLists['lists'];
+        foreach ($offices as $officename => $officeData) {
+          if (LegacyGroups::legacyGroupMatches($legacyGroup, $officename, $officeData)) {
+            foreach ($members as $member) {
+              $member = strtolower($member);
+              if (!in_array($member, $officeData['members'])) {
+                $memberships[$branch]['lists'][$officename]['members'][] = $member;
+              }
+            }
+            return TRUE;
+          }
+        }
+      }
+    }
+    return FALSE;
+  }
+
+  static protected function legacyGroupMatches($legacyGroup, $officename, $officeData) {
+    if ($legacyGroup == $officeData['properties']['group-email']) {
+      return TRUE;
+    }
+    return isset($officeData['properties']['alternate-addresses']) && in_array($legacyGroup, $officeData['properties']['alternate-addresses']);
+  }
+}
---
+++ tests/LegacyGroupsTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use Westkingdom\GoogleAPIExtensions\LegacyGroups;
+
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Dumper;
+
+class LegacyGroupsTestCase extends PHPUnit_Framework_TestCase {
+
+  public function arrayToYaml($data) {
+    if (is_string($data)) {
+      return trim($data);
+    }
+    else {
+      // Convert data to YAML
+      $dumper = new Dumper();
+      $dumper->setIndentation(2);
+      return trim($dumper->dump($data, PHP_INT_MAX));
+    }
+  }
+
+  public function testLegacyGroups() {
+    $testData = "
+west:
+  lists:
+    webminister:
+      members:
+        - minister@sca.org
+        - deputy@sca.org
+      properties:
+        group-name: 'West Kingdom Web Minister'
+        group-email: west-webminister@westkingdom.org
+        alternate-addresses:
+          - webminister@westkingdom.org";
+    $testMemberships = Yaml::parse(trim($testData));
+
+    // We are going to add two members to an existing group,
+    // and create a new legacy group with two members.
+    $testLegacy ="
+west-webminister@westkingdom.org   anotherwebguy@hitec.com
+webminister@westkingdom.org   deputywebdude@boilerstrap.com
+old-legacy@westkingdom.org   person1@somewhere.org,person2@somewhereelse.org";
+
+    $testMemberships = LegacyGroups::applyLegacyGroups($testMemberships, LegacyGroups::parseLegacyDreamHostGroups($testLegacy));
+
+    $expected = "
+west:
+  lists:
+    webminister:
+      members:
+        - minister@sca.org
+        - deputy@sca.org
+        - anotherwebguy@hitec.com
+        - deputywebdude@boilerstrap.com
+      properties:
+        group-name: 'West Kingdom Web Minister'
+        group-email: west-webminister@westkingdom.org
+        alternate-addresses:
+          - webminister@westkingdom.org
+_legacy:
+  lists:
+    old-legacy:
+      members:
+        - person1@somewhere.org
+        - person2@somewhereelse.org
+      properties:
+        group-email: old-legacy@westkingdom.org";
+
+    $this->assertEquals(trim($expected), $this->arrayToYaml($testMemberships));
+
+  }
+}
